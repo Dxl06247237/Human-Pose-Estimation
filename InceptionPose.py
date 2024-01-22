f28ad0887cb7a3688bbc3a229940f6011cb78f2b
Dxl06247237 <131754616+Dxl06247237@users.noreply.github.com>
2024-01-22 10:44:01 +0800

    Update InceptionPose.py

--- InceptionPose.py
+++ InceptionPose.py
@@ -1,10 +1,3 @@
-# ------------------------------------------------------------------------------
-# Copyright (c) Microsoft
-# Licensed under the MIT License.
-# Written by Bin Xiao (leoxiaobin@gmail.com)
-# Modified by Bowen Cheng (bcheng9@illinois.edu)
-# ------------------------------------------------------------------------------
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
